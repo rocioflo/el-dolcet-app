06c7852e7d020e8e5bd482c93b6b7b3b96f4cfaa
Rocio Flo <rocioflorencioytal@gmail.com>
2024-10-05 11:07:00 +0200

    Add spotify token api call test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,16 @@ import { Messages } from "./components/Messages/Messages";
 import { ProfileMenu } from "./components/ProfileMenu/ProfileMenu";
 
 export default function Home() {
+  // This is temporary, because a client component does not have access to env, but the test worked
+
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${process.env.SPOTIFY_CLIENT_ID}&client_secret=${process.env.SPOTIFY_CLIENT_SECRET}`,
+  }).then((data) => console.log(data));
+
   return (
     <div className="">
       <header className="h-12">
